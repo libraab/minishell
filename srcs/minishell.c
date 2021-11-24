3ba87e62b7a91346b34620c9ccde68e177aac46f
Asma Bouhlel boughdira <abouhlel@c1r4p2.42nice.fr>
2021-11-24 21:54:46 +0100

    after the game

--- srcs/minishell.c
+++ srcs/minishell.c
@@ -6,7 +6,7 @@
 /*   By: abouhlel <abouhlel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/25 10:58:24 by abouhlel          #+#    #+#             */
-/*   Updated: 2021/11/24 16:45:05 by abouhlel         ###   ########.fr       */
+/*   Updated: 2021/11/24 17:53:44 by abouhlel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -69,23 +69,23 @@ int	ft_prompt(char *entry, char **content, t_data *data)
 		i++;
 	}
 	//*******************************************************************************
-	// printf("\n{MY TOKENS}\n");
-	// for (int j = 0; j < data->nb ; j++)
-	// 	printf("[%d][%s]\n", data->token_tab[j].e_type, data->token_tab[j].value);
+	printf("\n{MY TOKENS}\n");
+	for (int j = 0; j < data->nb ; j++)
+		printf("[%d][%s]\n", data->token_tab[j].e_type, data->token_tab[j].value);
 	//*******************************************************************************
-	for (int k = 0; k < data->tot; k++)
-	{
-		printf("\n         {C M D}   \n");
-		printf("_________________________\n");
-		printf("| cmd        |   [%s]\n", data->cmd[k].cmd);
-		printf("_________________________\n");
-		for (int n = 0; data->cmd[k].full_cmd[n]; n++)
-			printf("| full cmd   |   [%s]\n", data->cmd[k].full_cmd[n]);
-		printf("_________________________\n");
-		for (int m = 0; data->cmd[k].redir[m]; m++)
-			printf("| redir      |   [%s]\n", data->cmd[k].redir[m]);
-		printf("_________________________\n");
-	}
+	// for (int k = 0; k < data->tot; k++)
+	// {
+	// 	printf("\n         {C M D}   \n");
+	// 	printf("_________________________\n");
+	// 	printf("| cmd        |   [%s]\n", data->cmd[k].cmd);
+	// 	printf("_________________________\n");
+	// 	for (int n = 0; data->cmd[k].full_cmd[n]; n++)
+	// 		printf("| full cmd   |   [%s]\n", data->cmd[k].full_cmd[n]);
+	// 	printf("_________________________\n");
+	// 	for (int m = 0; data->cmd[k].redir[m]; m++)
+	// 		printf("| redir      |   [%s]\n", data->cmd[k].redir[m]);
+	// 	printf("_________________________\n");
+	// }
 	//*******************************************************************************
 	ft_free(data);
 	free(entry);
